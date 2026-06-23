196260f5ed4d73bec20fef945d30aa4afc2d1af8
Daniel Lima <danielreis478@gmail.com>
2026-06-23 09:49:52 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
-  base: './'
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Painel Catavento',
+        short_name: 'Catavento',
+        description: 'Painel de Comunicação Alternativa',
+        theme_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'landscape',
+        icons: [
+          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icon-512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ]
 });
