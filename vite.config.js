f8f2c1c9c46e6d2e5ffd88983893e46b90ab7c94
Daniel Lima <danielreis478@gmail.com>
2026-06-24 11:06:12 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      injectRegister: 'inline',
       manifest: {
   "short_name": "Catavento",
   "name": "Catavento",
