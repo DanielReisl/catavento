736e8ff3af859657ea9524c66cdc6ba524d52517
Daniel Lima <danielreis478@gmail.com>
2026-06-24 07:39:42 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       manifest: {
   "short_name": "Catavento",
-  "name": "Painel Catavento",
+  "name": "Catavento",
   "icons": [
     { "src": "/icon-192.png", "type": "image/png", "sizes": "192x192", "purpose": "any maskable" },
     { "src": "/icon-512.png", "type": "image/png", "sizes": "512x512", "purpose": "any maskable" }
