f791a50af1ef5b0921ae96b7ed1248865d128244
Daniel Lima <danielreis478@gmail.com>
2026-06-24 11:50:01 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,19 +8,33 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: 'inline',
+      includeAssets: ['favicon.svg', 'icon-192.png', 'icon-512.png'],
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg}']
+      },
       manifest: {
-  "short_name": "Catavento",
-  "name": "Catavento",
-  "icons": [
-    { "src": "icon-192.png", "type": "image/png", "sizes": "192x192", "purpose": "any maskable" },
-    { "src": "icon-512.png", "type": "image/png", "sizes": "512x512", "purpose": "any maskable" }
-  ],
-  "start_url": "/",
-  "background_color": "#000000",
-  "theme_color": "#000000",
-  "display": "standalone",
-  "orientation": "portrait"
-}
+        short_name: "Catavento",
+        name: "Catavento",
+        icons: [
+          { 
+            src: "icon-192.png", 
+            type: "image/png", 
+            sizes: "192x192", 
+            purpose: "any maskable" 
+          },
+          { 
+            src: "icon-512.png", 
+            type: "image/png", 
+            sizes: "512x512", 
+            purpose: "any maskable" 
+          }
+        ],
+        start_url: "/",
+        background_color: "#000000",
+        theme_color: "#000000",
+        display: "standalone",
+        orientation: "portrait"
+      }
     })
   ]
 });
