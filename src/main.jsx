f69e7b293dcd35988751e0ddc3e59848c35a6d70
Daniel Lima <danielreis478@gmail.com>
2026-06-24 21:36:46 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-serif'
+import { createRoot as initRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { registerSW } from 'virtual:pwa-register'
 
-// Ativa o registro do Service Worker imediatamente em tempo de execução
 registerSW({ immediate: true })
 
-createRoot(document.getElementById('root')).render(
+initRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+)
