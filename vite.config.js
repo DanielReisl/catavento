d272aa75fdd0d5384b315895681562ed93cb865f
Daniel Lima <danielreis478@gmail.com>
2026-06-24 22:24:30 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,10 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      strategies: 'generateSW',
-      manifestFilename: 'manifest.json',
       registerType: 'autoUpdate',
-      includeAssets: ['icon-192.png', 'icon-512.png'],
+      strategies: 'generateSW',
       workbox: {
-        globPatterns: ['**/*.{js,css,html}', '*.png'], // <-- Ajustado para buscar o PNG na raiz do build
+        globPatterns: ['**/*.{js,css,html}', '*.png'],
         skipWaiting: true,
         clientsClaim: true,
         cleanupOutdatedCaches: true
@@ -20,8 +18,18 @@ export default defineConfig({
         short_name: "Catavento",
         name: "Catavento",
         icons: [
-          { src: "/icon-192.png", type: "image/png", sizes: "192x192", purpose: "any" },
-          { src: "/icon-512.png", type: "image/png", sizes: "512x512", purpose: "any" }
+          {
+            src: "icon-192.png",
+            type: "image/png",
+            sizes: "192x192",
+            purpose: "any"
+          },
+          {
+            src: "icon-512.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "any"
+          }
         ],
         start_url: "/",
         background_color: "#0d1b2a",
