bf4db1d589c4d6396f01ea8dbf96d24236634798
Daniel Lima <danielreis478@gmail.com>
2026-06-24 21:38:36 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-serif'
-import { createRoot as initRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { registerSW } from 'virtual:pwa-register'
 
 registerSW({ immediate: true })
 
-initRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
