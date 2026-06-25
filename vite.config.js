4b9c5ed9347a0b2b2295f031a012002b22183803
Daniel Lima <danielreis478@gmail.com>
2026-06-24 22:47:43 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,41 +8,16 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       strategies: 'generateSW',
-      buildBase: '/', // <-- Garante que os arquivos fiquem na raiz do build
-      manifestFilename: 'manifest.webmanifest',
       workbox: {
         globPatterns: ['**/*.{js,css,html}', '*.png'],
         skipWaiting: true,
         clientsClaim: true,
         cleanupOutdatedCaches: true
-      },
-      manifest: {
-        short_name: "Catavento",
-        name: "Catavento",
-        icons: [
-          {
-            src: "icon-192.png",
-            type: "image/png",
-            sizes: "192x192",
-            purpose: "any"
-          },
-          {
-            src: "icon-512.png",
-            type: "image/png",
-            sizes: "512x512",
-            purpose: "any"
-          }
-        ],
-        start_url: "/",
-        background_color: "#0d1b2a",
-        theme_color: "#0d1b2a",
-        display: "standalone",
-        orientation: "portrait"
       }
     })
   ],
   build: {
     assetsDir: 'assets',
-    emptyOutDir: true // <-- Limpa o cache do build anterior na Vercel antes de gerar o novo
+    emptyOutDir: true
   }
 });
